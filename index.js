6802fc7c59c8fad35609e7c1bab05a22e3408e40
TheAngryEnglishman <whorlowharry@gmail.com>
2022-03-20 15:30:30 +0100

    index completed

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const projectManagement = document.querySelector("#card3");
 const kitchenCard = document.querySelector("#card2");
 const bathRoom = document.querySelector("#card1");
+const menuButton = document.querySelector(".menu-button");
+const nav = document.querySelector("nav");
+
+let showMenu = false;
+menuButton.addEventListener("click", toggolmenu);
+
+function toggolmenu() {
+    if (!showMenu) {
+        menuButton.classList.add("close");
+        showMenu = true;
+        nav.style.top = "0px";
+    } else {
+        menuButton.classList.remove("close");
+        showMenu = false;
+        nav.style.top = "-1000px";
+    }
+}
+
+/* cards */
 
 let cards = {
     kitchen: false,
